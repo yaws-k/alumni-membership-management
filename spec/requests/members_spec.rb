3e9e9fb8c9bde73ed575373e5797f482b3168dca
Yosuke Kato <yaw@ginnojo.jp>
2022-10-18 00:16:29 +0900

    Add board member specs to members request spec

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -165,7 +165,7 @@ RSpec.describe 'Members', type: :request do
 
     let(:member) { user.member }
     let(:member2) { create(:member, year_id: member.year_id) }
-    let(:member3) { create(:member)}
+    let(:member3) { create(:member) }
 
     describe '#index' do
       it 'returns http success' do
@@ -234,7 +234,7 @@ RSpec.describe 'Members', type: :request do
         expect(response).to have_http_status(:success)
       end
 
-      it 'returns http success with different year member data' do
+      it 'returns http redirect with different year member data' do
         sign_in user
         get "/members/#{member3.id}/edit"
         expect(response).to redirect_to(members_path)
@@ -272,29 +272,120 @@ RSpec.describe 'Members', type: :request do
 
   # Board
   context 'board member' do
-    let(:member) { user.member.update(roles: %w[board]) }
+    before do
+      user.member.update(roles: %w[board])
+    end
+
+    let(:member) { user.member }
+    let(:member2) { create(:member, year_id: member.year_id) }
+    let(:member3) { create(:member) }
 
     describe '#index' do
       it 'returns http success' do
-        user.member.update(roles: %w[board])
         sign_in user
         get '/members'
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe '#show' do
+      it 'returns http success for own data' do
+        sign_in user
+        get "/members/#{member.id}"
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns http success for same year member data' do
+        sign_in user
+        get "/members/#{member2.id}"
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns http success for different year member data' do
+        sign_in user
+        get "/members/#{member3.id}"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe '#new' do
+      it 'returns http success' do
+        sign_in user
+        get '/members/new'
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe '#create' do
+      it 'returns http redirect (succeed with same year)' do
+        sign_in user
+        post '/members', params: {
+          member: create_member_param.merge(year_id: member.year_id)
+        }
+        expect(response).to redirect_to(member_path(Member.last))
+      end
+
+      it 'returns http redirect (succeed with different year)' do
+        sign_in user
+        post '/members', params: {
+          member: create_member_param.merge(year_id: member3.year_id)
+        }
+        expect(response).to redirect_to(member_path(Member.last))
+      end
+    end
+
+    describe '#edit' do
+      it 'returns http success for own data' do
+        sign_in user
+        get "/members/#{member.id}/edit"
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns http success with same year member data' do
+        sign_in user
+        get "/members/#{member2.id}/edit"
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns http success with different year member data' do
+        sign_in user
+        get "/members/#{member3.id}/edit"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe '#update' do
+      it 'returns http redirect to detailed page for own data' do
+        sign_in user
+        patch "/members/#{member.id}", params: { member: edit_member_param }
+        expect(response).to redirect_to(member_path(member))
+      end
+
+      it 'returns http redirect to detailed page for same year member data' do
+        sign_in user
+        patch "/members/#{member2.id}", params: { member: edit_member_param }
+        expect(response).to redirect_to(member_path(member2))
+      end
+
+      it 'returns http redirect to members_path for different year member data' do
+        sign_in user
+        patch "/members/#{member3.id}", params: { member: edit_member_param }
+        expect(response).to redirect_to(member_path(member3))
+      end
+    end
+
+    describe '#destroy' do
+      it 'returns http redirect' do
+        sign_in user
+        delete "/members/#{member.id}"
+        expect(response).to redirect_to(members_path)
       end
     end
   end
 
   # Admin
   context 'admin' do
-    let(:member) { user.member.update(roles: %w[admin]) }
-
-    describe '#index' do
-      it 'returns http success' do
-        user.member.update(roles: %w[admin])
-        sign_in user
-        get '/members'
-        expect(response).to have_http_status(:success)
-      end
-    end
+    # The same as board member.
+    # Details should be tested with system specs.
   end
 end
