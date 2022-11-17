387f785420feb2eaa4199f9b0e828fb53afbba19
Yosuke Kato <yaw@ginnojo.jp>
2022-11-17 23:33:02 +0900

    Add system spec 001

---
+++ spec/system/001s_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "001s", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  let(:user) { create(:user) }
+  let(:member) { user.member }
+
+  def login(user: nil)
+    visit new_user_session_path
+    expect(page).to have_text('Log in')
+
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'commit'
+
+    expect(page).to have_text('Signed in successfully.')
+    expect(page).to have_button('Logout')
+  end
+
+  context 'normal user' do
+    it 'is possible to login' do
+      login(user:)
+    end
+  end
+
+  context 'lead' do
+    it 'is possible to login' do
+      member.update(roles: %w[lead])
+      login(user:)
+    end
+  end
+
+  context 'board' do
+    it 'is possible to login' do
+      member.update(roles: %w[board])
+      login(user:)
+    end
+  end
+
+  context 'admin' do
+    it 'is possible to login' do
+      member.update(roles: %w[admin])
+      login(user:)
+    end
+  end
+end
