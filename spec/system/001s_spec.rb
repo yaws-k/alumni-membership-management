c4492fb0a87ecff4802af7ea27feaaf76a74ea17
Yosuke Kato <yaw@ginnojo.jp>
2022-11-18 00:49:12 +0900

    Refactor 001s with shared_examples

--- spec/system/001s_spec.rb
+++ spec/system/001s_spec.rb
@@ -7,42 +7,36 @@ RSpec.describe "001s", type: :system do
 
   include_context 'base user'
 
-  def login_check(user: nil)
-    visit new_user_session_path
-    expect(page).to have_text('Log in')
+  RSpec.shared_examples 'login check' do
+    it 'is possible to login' do
+      visit new_user_session_path
+      expect(page).to have_text('Log in')
 
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'commit'
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'commit'
 
-    expect(page).to have_text('Signed in successfully.')
-    expect(page).to have_button('Logout')
+      expect(page).to have_text('Signed in successfully.')
+      expect(page).to have_button('Logout')
+    end
   end
 
   context 'normal user' do
-    it 'is possible to login' do
-      login_check(user:)
-    end
+    it_behaves_like 'login check'
   end
 
   context 'lead' do
-    it 'is possible to login' do
-      member.update(roles: %w[lead])
-      login_check(user:)
-    end
+    before { member.update(roles: %w[lead]) }
+    it_behaves_like 'login check'
   end
 
   context 'board' do
-    it 'is possible to login' do
-      member.update(roles: %w[board])
-      login_check(user:)
-    end
+    before { member.update(roles: %w[board]) }
+    it_behaves_like 'login check'
   end
 
   context 'admin' do
-    it 'is possible to login' do
-      member.update(roles: %w[admin])
-      login_check(user:)
-    end
+    before { member.update(roles: %w[admin]) }
+    it_behaves_like 'login check'
   end
 end
