062e21c0dfacbd609338eaa3c75af6833e7f1836
Yosuke Kato <yaw@ginnojo.jp>
2022-11-25 00:23:27 +0900

    Move part of 010 to 012

--- spec/system/012s_spec.rb
+++ spec/system/012s_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe '010s', type: :system do
+RSpec.describe '012s', type: :system do
   before do
     driven_by(:rack_test)
   end
@@ -9,43 +9,50 @@ RSpec.describe '010s', type: :system do
   include_context 'login'
   let!(:address) { create(:address, :full_fields, member_id: member.id) }
 
-  RSpec.shared_examples 'address' do
+  RSpec.shared_examples 'postal code' do
     before do
       visit member_path(member)
       click_link('編集', href: edit_member_address_path(member, address))
     end
 
-    it 'converts address' do
-      fill_in('address_address1', with: '東京都千代田区有楽町２ー9-17')
-      fill_in('address_address2', with: 'カーサなんとか')
+    it 'converts postal code (1)' do
+      fill_in('address_postal_code', with: '１００ー0005')
       click_button('送信')
 
       within(id: 'postalAddress') do
-        expect(page).to have_text('東京都千代田区有楽町2-9-17')
-        expect(page).to have_text('カーサなんとか')
+        expect(page).to have_text('100-0005')
+      end
+    end
+
+    it 'converts postal code (2)' do
+      fill_in('address_postal_code', with: '１００0005')
+      click_button('送信')
+
+      within(id: 'postalAddress') do
+        expect(page).to have_text('100-0005')
       end
     end
   end
 
   context 'normal user' do
-    it_behaves_like 'address'
+    it_behaves_like 'postal code'
   end
 
   context 'lead' do
     before { member.update(roles: %w[lead]) }
 
-    it_behaves_like 'address'
+    it_behaves_like 'postal code'
   end
 
   context 'board' do
     before { member.update(roles: %w[board]) }
 
-    it_behaves_like 'address'
+    it_behaves_like 'postal code'
   end
 
   context 'admin' do
     before { member.update(roles: %w[admin]) }
 
-    it_behaves_like 'address'
+    it_behaves_like 'postal code'
   end
 end
