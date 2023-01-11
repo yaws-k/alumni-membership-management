58405ca46e3e411e1d44de85ad265f55bd7d442a
Yosuke Kato <yaw@ginnojo.jp>
2023-01-11 23:49:57 +0900

    Remove event creation link from normal user and lead

--- spec/system/044s_spec.rb
+++ spec/system/044s_spec.rb
@@ -7,6 +7,33 @@ RSpec.describe '044s', type: :system do
 
   include_context 'base user'
 
+  RSpec.shared_examples '044 reject event creation' do
+    before { click_link('イベント一覧', href: events_path) }
+
+    it 'does not show link to create event' do
+      expect(page).to_not have_link('イベント追加', href: new_event_path)
+    end
+
+    it 'rejects access to new event page' do
+      visit new_event_path
+      expect(current_path).to eq(redirected_path)
+    end
+  end
+
+  context 'normal user' do
+    let(:redirected_path) { member_path(member) }
+    include_context 'login'
+
+    it_behaves_like '044 reject event creation'
+  end
+
+  context 'lead' do
+    let(:redirected_path) { members_path }
+    include_context 'login as lead'
+
+    it_behaves_like '044 reject event creation'
+  end
+
   context 'board' do
     include_context 'login as board'
     before do
