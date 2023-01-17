b2ef9bd42f96d887daddc1b64fc4b3ffd3bf3fba
Yosuke Kato <yaw@ginnojo.jp>
2023-01-17 22:32:06 +0900

    Reorganize views to edit actual presence

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,7 @@
 class AttendancesController < ApplicationController
   def edit
     @attendance = Attendance.find(params[:id])
+    @event = @attendance.event
 
     check_roles(member_id: @attendance.member_id)
     return if performed?
@@ -15,7 +16,12 @@ class AttendancesController < ApplicationController
     return if performed?
 
     attendance.update!(attendance_params)
-    redirect_to member_path(attendance.member_id, anchor: helpers.dom_id(attendance))
+
+    if params[:return_path].present?
+      redirect_to("#{params[:return_path]}#member_#{attendance.member_id}")
+    else
+      redirect_to member_path(attendance.member_id, anchor: helpers.dom_id(attendance))
+    end
   end
 
   private
