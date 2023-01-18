a617b9ab1aff6aee9c69f9c4ee1ba1cfd641249b
Yosuke Kato <yaw@ginnojo.jp>
2023-01-18 22:26:15 +0900

    #11 Add event participant download link

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -11,6 +11,26 @@ class ExportsController < ApplicationController
     end
   end
 
+  def event_participants
+    event = Event.find(params[:event_id])
+    applied = event.attendances.where(application: true).pluck(:id)
+    present = event.attendances.where(presence: true).pluck(:id)
+    attendances = Attendance.in(id: (applied + present)).index_by(&:member_id)
+    members = Member.year_sort(id: attendances.keys, order: :asc)
+    years = Year.order(anno_domini: :desc).index_by(&:id)
+    counts = {
+      application: event.attendances.where(application: true).size,
+      presence: event.attendances.where(presence: true).size
+    }
+
+    docs = { event:, attendances:, members:, years:, counts: }
+
+    workbook = RubyXL::Parser.parse("#{Rails.root}/lib/assets/event_participants_template.xlsx")
+    fill_participants_sheet(workbook:, docs:)
+
+    send_data(workbook.stream.read, filename: "participants_#{Date.today}.xlsx", disposition: 'attachment')
+  end
+
   def members
     years = Year.accessible_years(roles: @roles, current_user:).pluck(:id, :graduate_year).to_h
     members = Member.in(year_id: years.keys).order(search_key: :asc)
@@ -69,6 +89,43 @@ class ExportsController < ApplicationController
       names[m.id] = "#{m.family_name} #{m.first_name}"
     end
     names
+  end
+
+  #
+  # event participants
+  #
+  def event_presence(flag)
+    case flag
+    when true
+      '出席'
+    when false
+      '欠席'
+    else
+      '未入力'
+    end
+  end
+
+  def fill_participants_sheet(workbook:, docs:)
+    # docs = { event:, attendances:, members:, years:, counts: }
+    workbook[0].add_cell(0, 1, docs[:event].event_name)
+    workbook[0].add_cell(1, 1, docs[:event].event_date.to_s.tr('-', '/'))
+    workbook[0].add_cell(2, 1, docs[:event].fee)
+
+    workbook[0].add_cell(4, 1, docs[:counts][:application])
+    workbook[0].add_cell(5, 1, docs[:counts][:presence])
+
+    workbook[0].add_cell(7, 4, Time.now.localtime.strftime('%F %T'))
+
+    row = 9
+    docs[:members].each do |member|
+      workbook[0].add_cell(row, 0, docs[:years][member.year_id].graduate_year)
+      workbook[0].add_cell(row, 1, ApplicationController.helpers.full_name(member)[0])
+      workbook[0].add_cell(row, 2, event_presence(docs[:attendances][member.id].application))
+      workbook[0].add_cell(row, 3, event_presence(docs[:attendances][member.id].presence))
+      workbook[0].add_cell(row, 4, docs[:attendances][member.id].note)
+      workbook[0].add_cell(row, 5, docs[:attendances][member.id].payment_date.to_s.tr('-', '/'))
+      row += 1
+    end
   end
 
   #
